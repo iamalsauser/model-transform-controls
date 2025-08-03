df3f5027dfccb1c0629531a0dbee0d3cdef60b8c
iamalsauser <parthsinhv4@gmail.com>
2025-08-03 23:56:47 +0530

    Initial Commit

---
+++ model-tranform-controls/model_tranform_controlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  model_tranform_controlsApp.swift
+//  model-tranform-controls
+//
+//  Created by Parth Sinh on 03/08/25.
+//
+
+import SwiftUI
+
+@main
+struct model_tranform_controlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ model-tranform-controlsTests/model_tranform_controlsTests.swift
@@ -0,0 +1,16 @@
+//
+//  model_tranform_controlsTests.swift
+//  model-tranform-controlsTests
+//
+//  Created by Parth Sinh on 03/08/25.
+//
+
+import Testing
+
+struct model_tranform_controlsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
